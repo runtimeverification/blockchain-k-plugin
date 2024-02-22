03929f67b76c8931eddbf473db92d85f8c11cece
Dwight Guth <dwight.guth@runtimeverification.com>
2024-02-22 17:00:55 -0600

    add functions to header file

--- plugin-c/blake2.h
+++ plugin-c/blake2.h
@@ -5,5 +5,9 @@
 
 void blake2b_compress(uint64_t *h, uint64_t *m, uint64_t *t, char f,
                       uint32_t rounds);
+void blake2b_compress_generic(uint64_t *h, uint64_t *m, uint64_t *t, char f,
+                              uint32_t rounds);
+void blake2b_compress_avx2(uint64_t *h, uint64_t *m, uint64_t *t, char f,
+                           uint32_t rounds);
 
 #endif
