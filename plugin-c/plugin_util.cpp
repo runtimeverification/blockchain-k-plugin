c9698c764521e71d9b8049eb41e4cc2a46343c64
Dwight Guth <dwight.guth@runtimeverification.com>
2024-03-14 05:24:40 -0500

    Update k (#166)
    * reformat
    
    * fix names for latest version of llvm backend
    
    * update k version
    
    * more updated symbol names
    
    * update output files

--- plugin-c/plugin_util.cpp
+++ plugin-c/plugin_util.cpp
@@ -1,8 +1,9 @@
 #include "plugin_util.h"
 
 extern "C" {
-string* allocString(size_t len) {
-  struct string *result = (struct string *)koreAllocToken(len + sizeof(string));
+string *allocString(size_t len) {
+  struct string *result =
+      (struct string *)kore_alloc_token(len + sizeof(string));
   init_with_len(result, len);
   return result;
 }
@@ -14,7 +15,7 @@ string *hexEncode(unsigned char *digest, size_t len) {
   for (size_t i = 0, j = 0; i < len; i++, j += 2) {
     snprintf(byte, sizeof(byte), "%02x", digest[i]);
     result->data[j] = byte[0];
-    result->data[j+1] = byte[1];
+    result->data[j + 1] = byte[1];
   }
   return result;
 }
