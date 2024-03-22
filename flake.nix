59418910091fae03152171601e21691623b33168
Jost Berthold <jost.berthold@gmail.com>
2024-03-22 14:16:37 +1100

    move blake2.a artefact to build dir, adapt flake to build/provide it

--- flake.nix
+++ flake.nix
@@ -80,7 +80,7 @@
             ${
               lib.strings.optionalString (stdenv.isAarch64 && stdenv.isDarwin)
               "APPLE_SILICON=true"
-            } make libcryptopp libff
+            } make build libcryptopp libff
           '';
           installPhase = ''
             mkdir -p $out/include
