e16f2aed2694f8db444d6ed7b9e825e94d554458
Maria Kotsifakou <kotsifakou.maria@gmail.com>
2025-02-05 14:42:53 -0600

    Removed exceptions.
    Preconditions are testing for these errors, and
    verify_kzg_proof sets &ref to false if it fails to verify.

--- plugin-c/kzg.cpp
+++ plugin-c/kzg.cpp
@@ -35,18 +35,6 @@ bool hook_KRYPTO_verifyKZGProof(struct string *commitment, struct string *z,
     setup(&settings);
     once = false;
   }
-  if (len(commitment) != 48) {
-    throw std::invalid_argument("Length of commitment is not 48 bytes");
-  }
-  if (len(z) != 32) {
-    throw std::invalid_argument("Length of z field is not 32 bytes");
-  }
-  if (len(y) != 32) {
-    throw std::invalid_argument("Length of y field is not 32 bytes");
-  }
-  if (len(proof) != 48) {
-    throw std::invalid_argument("Length of proof is not 48 bytes");
-  }
   bool res;
   verify_kzg_proof(&res, (Bytes48 *)&commitment->data[0],
                    (Bytes32 *)&z->data[0], (Bytes32 *)&y->data[0],
